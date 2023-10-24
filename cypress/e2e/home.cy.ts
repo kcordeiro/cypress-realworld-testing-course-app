c1a31e394cb27d5087c6b9d1f7f3d75da57c5f85
Kasey Cordeiro Brown <49873686+kcordeiro@users.noreply.github.com>
2023-10-23 20:11:15 -0400

    Set up Cypress

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,13 @@
+describe("home page", () => {
+  it("the h1 contains the correct text", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("[data-test='hero-heading']")
+      .should("be.visible")
+      .contains("Testing Next.js Applications with Cypress")
+  })
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt")
+      .should("be.visible")
+  })
+})
