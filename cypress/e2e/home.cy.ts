00ca89b0c9b95ac0d3a75ba663c8eb265292390d
Kasey Cordeiro <kasey.cordeiro@tomorrow.io>
2023-10-25 09:54:55 -0400

    Add command to get data-test attributes

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,17 @@
 describe("home page", () => {
-  it("the h1 contains the correct text", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000")
-    cy.get("[data-test='hero-heading']")
+  })
+
+  it.only("the h1 contains the correct text", () => {
+    cy.getByData("hero-heading")
       .should("be.visible")
       .contains("Testing Next.js Applications with Cypress")
   })
-  it.only("the features on the homepage are correct", () => {
-    cy.visit("http://localhost:3000")
-    cy.get("dt")
-      .should("be.visible")
+
+  it("the features on the homepage are correct", () => {
+    cy.get("dt").should("be.visible").eq(0).contains("4 Courses")
+    cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
+    cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
   })
 })
