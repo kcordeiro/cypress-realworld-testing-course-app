4d8aad73068a624066ea6988e39aacdb894d9af6
Kasey Cordeiro <kasey.cordeiro@tomorrow.io>
2023-10-25 10:06:39 -0400

    Add check for duplicate email subscription

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -8,4 +8,16 @@ describe("Newsletter Subscribe Form", () => {
     cy.getByData("submit-button").click()
     cy.getByData("success-message").should("exist").contains("tom@aol.com")
   })
+
+  it("does NOT allow invalid email address", () => {
+    cy.getByData("email-input").should("be.visible").type("tom")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it("does NOT allow duplicate subscription", () => {
+    cy.getByData("email-input").should("be.visible").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message").should("exist").contains("already exists")
+  })
 })
