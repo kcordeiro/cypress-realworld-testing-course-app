2fd5c6e5ace31076da766c1d0f3da64f1c5d8b7b
Kasey Cordeiro <kasey.cordeiro@tomorrow.io>
2023-10-25 11:45:46 -0400

    Add tests for course page paths
    Update prettier config and add script to run

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,23 +1,25 @@
-describe("Newsletter Subscribe Form", () => {
+describe('Newsletter Subscribe Form', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000")
-  })
+    cy.visit('http://localhost:3000');
+  });
 
-  it("allows users to subscribe to the email list", () => {
-    cy.getByData("email-input").should("be.visible").type("tom@aol.com")
-    cy.getByData("submit-button").click()
-    cy.getByData("success-message").should("exist").contains("tom@aol.com")
-  })
+  it('allows users to subscribe to the email list', () => {
+    cy.getByData('email-input').should('be.visible').type('tom@aol.com');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message').should('exist').contains('tom@aol.com');
+  });
 
-  it("does NOT allow invalid email address", () => {
-    cy.getByData("email-input").should("be.visible").type("tom")
-    cy.getByData("submit-button").click()
-    cy.getByData("success-message").should("not.exist")
-  })
+  it('does NOT allow invalid email address', () => {
+    cy.getByData('email-input').should('be.visible').type('tom');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message').should('not.exist');
+  });
 
-  it("does NOT allow duplicate subscription", () => {
-    cy.getByData("email-input").should("be.visible").type("john@example.com")
-    cy.getByData("submit-button").click()
-    cy.getByData("server-error-message").should("exist").contains("already exists")
-  })
-})
+  it('does NOT allow duplicate subscription', () => {
+    cy.getByData('email-input').should('be.visible').type('john@example.com');
+    cy.getByData('submit-button').click();
+    cy.getByData('server-error-message')
+      .should('exist')
+      .contains('already exists');
+  });
+});
